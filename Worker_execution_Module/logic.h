672c59c7cfe5eb4d4549cced2fa04d73cc1a3af9
JerMa25 <tchamijerry@gmail.com>
2026-05-28 17:54:03 +0100

    delete files

--- Worker_execution_Module/logic.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOGIC_H
-#define LOGIC_H
-
-
-typedef void *(*fn)(void *);
-fn matcher(char *name);
-
-#endif
--- Worker_execution_Module/scratch/logic.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int worker_entry() {
-    printf("Coming from logic\n");
-    _exit(0);
-}
